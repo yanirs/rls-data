38594af52001efa12a9f0e1c02dc46a9073a58a4
Yanir Seroussi <rls@yanirseroussi.com>
2022-04-22 18:10:55 +0300

    Download AODN files

--- rls/downloader.py
+++ rls/downloader.py
@@ -12,10 +12,13 @@ _logger = logging.getLogger("rls.processor")
 def download_survey_data(survey_data_dir: Path) -> None:
     """Download RLS CSV data files to the given directory, creating it if it doesn't exist."""
     verify_empty_dir(survey_data_dir)
-    for data_type in ("M1", "M2_INVERT", "M2_FISH"):
+    # TODO: limit to only the RLS program? `&CQL_FILTER=(program%20LIKE%20'RLS')`
+    # TODO: parallelise?
+    # TODO: drop m0?
+    for data_type in ("m0_off_transect_sighting", "m1", "m2_cryptic_fish", "m2_inverts"):
         url = (
-            "http://geoserver-rls.imas.utas.edu.au/geoserver/wfs?SERVICE=WFS&outputFormat=csv&"
-            f"REQUEST=GetFeature&VERSION=1.0.0&typeName=RLS:{data_type}_DATA"
+            "https://geoserver-portal.aodn.org.au/geoserver/ows?SERVICE=WFS&outputFormat=csv&REQUEST=GetFeature&"
+            f"VERSION=1.0.0&CQL_FILTER=(program%20LIKE%20'RLS')&typeName=imos:ep_{data_type}_public_data"
         )
         out_path = survey_data_dir / f"{data_type.lower()}.csv"
         _logger.info(f"Downloading {url} to {out_path}")
