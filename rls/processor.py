92d21cbfd8068260094708892309ecf0ec7d946d
Yanir Seroussi <rls@yanirseroussi.com>
2023-03-26 21:32:30 +0000

    Update docstring

--- rls/processor.py
+++ rls/processor.py
@@ -92,10 +92,16 @@ def _create_site_summaries(survey_data: pd.DataFrame, dst_dir: Path) -> None:
     """
     Create the site summaries from survey_data and write them in API JSON format to dst_dir.
 
-    Two files will be created, api-site-surveys.json and api-site-surveys.min.json, where the latter is the same JSON
+    Two legacy files are created, api-site-surveys.json and api-site-surveys.min.json, where the latter is the same JSON
     as the former but without pretty-printing whitespace. The content of the files is the same mapping from site code
     to [realm: str, ecoregion: str, site_name: str, longitude: float, latitude: float, num_surveys: int,
     species_name_to_num_surveys: dict[str, int]]
+
+    In addition, two new format files are created:
+      - sites.json: site data with keys 'rows' - array of arrays, and 'keys' - specifying the meaning of each array
+        value in 'rows'
+      - surveys.json: mapping from species_name (str) to site_code (str) to count (int), which is the number of
+        observations of the species at the site
     """
     site_survey_counts = survey_data.groupby("site_code")["survey_id"].nunique()
     site_survey_counts.name = "num_surveys"
