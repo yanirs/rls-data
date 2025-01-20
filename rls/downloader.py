1a5c70dec0f5d589fc6cbb35d104f098819e3753
Yanir Seroussi <3952615+yanirs@users.noreply.github.com>
2025-01-21 08:47:28 +1000

    Improve downloader error checking (#56)

--- rls/downloader.py
+++ rls/downloader.py
@@ -14,7 +14,7 @@ _logger = logging.getLogger("rls.processor")
 def download_survey_data(survey_data_dir: Path) -> None:
     """Download RLS CSV data files to the given directory, creating it if needed."""
     verify_empty_dir(survey_data_dir)
-    ThreadPoolExecutor(max_workers=3).map(
+    results = ThreadPoolExecutor(max_workers=3).map(
         _download_survey_data_file,
         [
             (
@@ -33,12 +33,16 @@ def download_survey_data(survey_data_dir: Path) -> None:
         # Five minutes should be plenty of time to download the largest file (m1).
         timeout=300,
     )
+    for _ in results:
+        pass
 
 
 def _download_survey_data_file(url_and_out_path: tuple[str, Path]) -> None:
     """Download a single survey data file."""
     url, out_path = url_and_out_path
     _logger.info("Downloading %s to %s", url, out_path)
+    response = requests.get(url, timeout=timedelta(minutes=10).total_seconds())
+    response.raise_for_status()
     with out_path.open("w") as fp:
-        fp.write(requests.get(url, timeout=timedelta(minutes=10).total_seconds()).text)
+        fp.write(response.text)
     _logger.info("Saved %s", out_path)
